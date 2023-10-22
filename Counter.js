79c97e90798a2e6982f86150692e91e3b2f82cb6
Sumit <jha.07sumit2003@gmail.com>
2023-10-22 22:02:35 +0530

    Added Counter

---
+++ Counter.js
@@ -0,0 +1,24 @@
+// #Question:-
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+// #Solution:-
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+
+    let counter = n;
+
+    return () => counter++;
+};
+
+
+const counter = createCounter(10);
+
+
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter()); 
